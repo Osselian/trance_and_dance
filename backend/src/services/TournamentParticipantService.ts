304023c19ba3a59a3826667d7cdfe389d6cf4cf8
Osselian <rossemir11@gmail.com>
2025-05-24 21:29:05 +0400

    add tournament username

--- backend/src/services/TournamentParticipantService.ts
+++ backend/src/services/TournamentParticipantService.ts
@@ -1,13 +1,18 @@
 import { TournamentParticipantRepository } from 
 	"../repositories/TournamentParticipantRepository";
 import { TournamentParticipant } from "@prisma/client";
+import { RegisterParticipantDto}  from "../dtos/TournamentParticipantDto";
+import { dot } from "node:test/reporters";
 
 export class TournamentParticipantService {
 	constructor(
 		private participantRepo = new TournamentParticipantRepository()
 	) {}
 
-	async register(tournamentId: number, userId: number): Promise<TournamentParticipant> {
+	async register(tournamentId: number, userId: number, dto?: RegisterParticipantDto): 
+		Promise<TournamentParticipant> 
+	{
+		const tournamentName = dto?.tournamentName;
 		//TODO: проверка дедлайна, уникальности, прав доступа
 		return this.participantRepo.register(tournamentId, userId);
 	}
