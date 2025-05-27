132354ceb932387bbf2ab1f86a7111faf18b7575
Osselian <rossemir11@gmail.com>
2025-05-28 00:05:40 +0400

    mutex and loses

--- backend/src/services/UserService.ts
+++ backend/src/services/UserService.ts
@@ -45,6 +45,38 @@ export class UserService {
 		return this.userRepo.updateUserProfile(userId, updateData);
 	}
 
+	async updateWins(userId: number) {
+		try {
+			const user = await this.userRepo.findById(userId);
+			if (!user)
+				throw new Error('User not found');
+
+			const updData = {wins: user.wins + 1};
+			this.userRepo.updateUserProfile(userId, updData);
+		}
+		catch (err) {
+			const msg = err instanceof Error ? err.message : 'Error';
+			console.error(`[UserService] updateWins: ${msg}`);
+			throw new Error(msg);
+		}
+	}
+
+	async updateLoses(userId: number) {
+		try {
+			const user = await this.userRepo.findById(userId);
+			if (!user)
+				throw new Error('User not found');
+
+			const updData = {losses: user.losses + 1};
+			this.userRepo.updateUserProfile(userId, updData);
+		}
+		catch (err) {
+			const msg = err instanceof Error ? err.message : 'Error';
+			console.error(`[UserService] updateWins: ${msg}`);
+			throw new Error(msg);
+		}
+	}
+
 	static markUserOnline(userId: number) {
 		console.log(`[UserService] markUserOnline: ${userId}`);
 		onlineUsers.add(userId);
